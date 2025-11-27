96ba4e99b40dc869518f28501d1fea11a6456602
Juan <juan_noe10hotmail.com>
2025-11-26 23:02:31 -0400

    tst2

--- vite.config.js
+++ vite.config.js
@@ -1,27 +1,35 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
 import { resolve } from 'path';
 
-export default defineConfig({
-    plugins: [
-        laravel({
-            input: 'resources/js/app.js',
-            refresh: true,
-        }),
-        vue({
-            template: {
-                transformAssetUrls: {
-                    base: null,
-                    includeAbsolute: false,
+export default defineConfig(({ command, mode }) => {
+    const env = loadEnv(mode, process.cwd(), '');
+
+    // Define la ruta base para la compilación
+    const build_base = env.VITE_BUILD_BASE_PATH ? `/${env.VITE_BUILD_BASE_PATH}/` : '/build/';
+    const base = command === 'build' ? build_base : '/';
+
+    return {
+        base: base,
+        plugins: [
+            laravel({
+                input: 'resources/js/app.js',
+                refresh: true,
+            }),
+            vue({
+                template: {
+                    transformAssetUrls: {
+                        base: null,
+                        includeAbsolute: false,
+                    },
                 },
+            }),
+        ],
+        resolve: {
+            alias: {
+              '@': resolve(__dirname, 'resources/js'),
             },
-        }),
-    ],
-    base: 'http://localhost:8000/build',
-    resolve: {
-        alias: {
-          '@': resolve(__dirname, 'resources/js'),
         },
-      },
+    }
 });
